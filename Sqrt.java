20721f6c23c31c422dee1b93cfdb2417da2217f0
doniyor9936 <103168279+DoniyorQalandarov@users.noreply.github.com>
2022-04-08 21:19:49 +0500

    Add files via upload

---
+++ Sqrt.java
@@ -0,0 +1,8 @@
+package leetCodeExamples;
+
+public class Sqrt {
+    public static void main(String[] args) {
+        int number = (int) Math.sqrt(4);
+        System.out.println(number);
+    }
+}
